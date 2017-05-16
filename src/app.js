e40856b5ad581fa6506048a0675038f9e0e5145e
Julian Scharf <jscharf@thoughtworks.com>
2017-05-16 16:11:22 +1000

    [Julian Scharf] Initial commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+// npm install --save-dev eslint-config-rallycoding
+
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers'
+
+const App = () => {
+  return (
+    <Provider store={createStore}>
+      <View>
+       <Text> Potato </Text>
+      </View>
+    </Provider>
+  );
+};
+
+export default App;
