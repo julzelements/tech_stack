ca192c08a627a6522163de3305dffdf1b87ea5fa
Julian Scharf <jscharf@thoughtworks.com>
2017-05-17 09:50:22 +1000

    [Julian Scharf] 88 MapStateToProps

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
 // npm install --save-dev eslint-config-rallycoding
 
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import reducers from './reducers'
+import reducers from './reducers';
+import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
 
 const App = () => {
   return (
-    <Provider store={createStore}>
+    <Provider store={createStore(reducers)}>
       <View>
-       <Text> Potato </Text>
+        <Header headerText="Tech Stack" />
+        <LibraryList />
       </View>
     </Provider>
   );
